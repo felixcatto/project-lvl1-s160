8766199517a443f06469786232f287a679652349
felixcatto <felix0808@yandex.ru>
2017-11-02 15:40:29 +0400

    add fifth task

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,3 +1,32 @@
-export function startCalcGame() {
-  console.log('started');
-}
+import { startGame } from '../index';
+
+
+const operations = ['+', '-', '*'];
+
+const gameOptions = {
+  description: 'What is the result of the expression?',
+  makeQuestion: () => {
+    const num1 = Math.round(Math.random() * 10);
+    const num2 = Math.round(Math.random() * 10);
+    const operation = operations[Math.floor(Math.random() * 3)];
+    return `${num1} ${operation} ${num2}`;
+  },
+  getCorrectAnswer: (question) => {
+    const parts = question.split(' ');
+    const num1 = +parts[0];
+    const operation = parts[1];
+    const num2 = +parts[2];
+    let result;
+    if (operation === '+') {
+      result = num1 + num2;
+    } else if (operation === '-') {
+      result = num1 - num2;
+    } else if (operation === '*') {
+      result = num1 * num2;
+    }
+
+    return result.toString();
+  },
+};
+
+export const startCalcGame = () => startGame(gameOptions);
